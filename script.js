34fdb6c95df659f5cb20d5b6878e90c052fe4ac9
micoledraco3322 <40352401+micoledraco3322@users.noreply.github.com>
2021-08-15 10:10:08 +0800

    Add files via upload
    New changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//List of quotes
+
 const sample =
 
 [
@@ -23,27 +25,57 @@ const sample =
  },
 
  {
-  "quote": "Java is to JavaScript what car is to Carpet.",
-  "by": "- Chris Heilmann"
+  "quote": "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
+  "by": "- Martin Luther King Jr."
+ },
+ 
+ {
+  "quote": "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+  "by": "- Mahatma Gandhi"
+ },
+ 
+ {
+  "quote": "I have not failed. I've just found 10,000 ways that won't work.",
+  "by": "- Thomas A. Edison"
+ },
+ 
+ {
+  "quote": "I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+  "by": "- Albert Einstein"
+ },
+ 
+ {
+  "quote": "It is never too late to be what you might have been.",
+  "by": "- George Elliot"
+ },
+ 
+ {
+  "quote": "There is no greater agony than bearing an untold story inside you.",
+  "by": "- Maya Angelou"
+ },
+ 
+ {
+  "quote": "Life isn't about finding yourself. Life is about creating yourself.",
+  "by": "- George Bernard Shaw"
  }
 
 ]
 
+//Text/value holder a.k.a "Global variable"
 var storeQ = "Text";
 var storeA = "Text";
 
+
+//Functions for storing the text and displaying the text
 function randomizer() {
-
+	//Count how many quotes are there in the list
 	const count = sample.length
-	let intStore = 0
 
- 	for (let i = 0; i <= count; i++)
- 	{
-    	  intStore=i
- 	}
-
-	randomInt = Math.floor((Math.random()*intStore)+1)
-
+	//Generate random number for generating random text/quote base on the list
+	randomInt = Math.floor((Math.random()*count)+1)
+	
+	//removes the "undefined" from the list
+	//avoids from displaying "undefined"
 	if (typeof sample[randomInt] === "undefined")
 	{
 	  randomizer()
@@ -55,13 +87,15 @@ function randomizer() {
 }
 randomizer()
 
+//Generate and display new text upon pressing the button
 function generate() {
    randomizer();
    document.getElementById("text").innerHTML = storeQ;
    document.getElementById("by").innerHTML = storeA;
 }
 
-function beta() {
+//Display text in "div.quote"
+function show() {
    document.getElementById("text").innerHTML = storeQ;
    document.getElementById("by").innerHTML = storeA;
 }
