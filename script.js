f5d132527bcd55ef04dd99e90a9494130b841c4b
micoledraco3322 <40352401+micoledraco3322@users.noreply.github.com>
2026-09-06 15:53:44 +0000

    error found. Missing "."

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarMenu = document.querySelector('navbar-menu');
+const navbarMenu = document.querySelector('.navbar-menu');
 
 navbarToggle.addEventListener('click',() => {
     navbarToggle.classList.toggle('active');
